991dcb594057fb6e5c0ad3b74e63dfff87e99737
Riroaki <2569126376@qq.com>
2018-12-29 15:54:15 +0800

    Fixed bug of decrypted reading.

--- part5/file.c
+++ part5/file.c
@@ -70,8 +70,9 @@ ssize_t new_sync_write_crypt(struct file *filp, const char __user *buf, size_t l
 	for(i=0;i<len;i++){
 		mybuf[i] = (mybuf[i] + 25)%128;
 	}
+	copy_to_user(buf, mybuf, len);
 	printk("haha encrypt %ld\n", len);
-	return new_sync_write(filp, mybuf, len, ppos);
+	return new_sync_write(filp, buf, len, ppos);
 }
 
 static ssize_t new_sync_read_crypt(struct file *filp, char __user *buf, size_t len, loff_t *ppos)
@@ -79,10 +80,12 @@ static ssize_t new_sync_read_crypt(struct file *filp, char __user *buf, size_t l
         int i;
 	char* mybuf = (char*)kmalloc(sizeof(char)*len,GFP_KERNEL);
         ssize_t ret = new_sync_read(filp, buf, len, ppos);
+	copy_from_user(mybuf, buf, len);
 	for(i=0;i<len;i++){
 		mybuf[i] = (mybuf[i] - 25 + 128)%128;
 	}
-	printk("haha encrypt %ld\n", len);
+	copy_to_user(buf, mybuf, len);
+	printk("haha decrypt %ld\n", len);
         return ret;
 }
 
